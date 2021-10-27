040113caed530b61e012a5066d85769d761913a0
k-ballard <86800230+k-ballard@users.noreply.github.com>
2021-10-27 16:35:45 -0700

    Working Earth Spike proc roll function

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-let hit = 0;
+let placeholder = 0;
+let earthSpikes = .2;
+let northernWinds = .2;
+let lightningBolt = .2;
+
+/* Don't think this will work
+function hit(x,y,z) {
+    
+};
+*/
+
+function rollEarthSpikes(x) {
+    x = earthSpikes;
+    return Math.random() < x
+};
+
+console.log(rollEarthSpikes());

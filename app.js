66424c582fb837ceb616976bd3316fc6809d8f18
k-ballard <86800230+k-ballard@users.noreply.github.com>
2021-10-27 17:58:01 -0700

    Cleaned up some of the naming. Might need to create objects and use getters/setters when calculating procs.

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-let placeholder = 0;
-let earthSpikes = .2;
-let northernWinds = .2;
-let lightningBolt = .2;
 
-/* Don't think this will work
-function hit(x,y,z) {
-    
-};
-*/
+let earthSpikesProcChance = .2;
+let northernWindsProcChance = .2;
+let lightningBoltProcChance = .2;
+
+let earthSpikesProc = false;
+let northerWindsProc = false;
+let lightningBoltProc = false;
 
 function rollEarthSpikes(x) {
     x = earthSpikes;
@@ -24,35 +22,41 @@ function rollLightningBolt(x) {
     return Math.random() < x;
 };
 
+/* Might be on to something here...
+function secondaryEffectsProc() {
+    if ()
+}
+*/
+
 /* Tests for proc rolls */
 console.log("Earth Spike hit: " + rollEarthSpikes());
 console.log("Northern Winds hit: " + rollNorthernWinds());
 console.log("Lightning Bolt hit: " + rollLightningBolt());
 
 
-let hailstorm = false;
-let chargedStone = false;
-let tempest = false;
-let allSecondaryProcs = false;
-let noSecondaryProcs = false;
+let hailstorm = 'No';
+let chargedStone = 'No';
+let tempest = 'No';
+let allTertiaryProcs = 'No';
+let noTertiaryProcs = 'No';
 
-function secondaryEffectProc() {
-    if(rollEarthSpikes() == true && rollNorthernWinds() == true && rollLightningBolt() == false) {
-        return hailstorm = true;
-    } else if (rollEarthSpikes() == false && rollNorthernWinds() == true && rollLightningBolt() == true) {
-        return tempest = true;
-    } else if (rollEarthSpikes() == true && rollNorthernWinds() == false && rollLightningBolt() == true) {
-        return chargedStone = true;
-    } else if (rollEarthSpikes() == true && rollNorthernWinds() == true && rollLightningBolt() == true) {
-        return allSecondaryProcs = true;
-    } else if (rollEarthSpikes() == false && rollNorthernWinds() == false && rollLightningBolt() == false) {
-        return noSecondaryProcs = true;
+function tertiaryEffectProc() {
+    if(rollEarthSpikes() === true && rollNorthernWinds() === true && rollLightningBolt() === false) {
+        return hailstorm = 'Yes';
+    } else if (rollEarthSpikes() === false && rollNorthernWinds() === true && rollLightningBolt() === true) {
+        return tempest = 'Yes';
+    } else if (rollEarthSpikes() === true && rollNorthernWinds() === false && rollLightningBolt() === true) {
+        return chargedStone = 'Yes';
+    } else if (rollEarthSpikes() === true && rollNorthernWinds() === true && rollLightningBolt() === true) {
+        return allSecondaryProcs = 'Yes';
+    } else if (rollEarthSpikes() === false && rollNorthernWinds() === false && rollLightningBolt() === false) {
+        return noSecondaryProcs = 'Yes';
     } else {
         console.log('Something went wrong...');
     }
 };
 
-secondaryEffectProc();
+tertiaryEffectProc();
 console.log('Does Hailstorm proc? ' + hailstorm);
 console.log('Does Tempest proc? ' + tempest);
 console.log('Does Charged Stone proc? ' + chargedStone);

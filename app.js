26b9b938aeeb4ca0484bcd40cab49c86a0d28e32
k-ballard <86800230+k-ballard@users.noreply.github.com>
2021-10-27 17:29:28 -0700

    Added secondary proc function -- still buggy

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function hit(x,y,z) {
 
 function rollEarthSpikes(x) {
     x = earthSpikes;
-    return Math.random() < x
+    return Math.random() < x;
 };
 
 function rollNorthernWinds(x) {
@@ -21,9 +21,40 @@ function rollNorthernWinds(x) {
 
 function rollLightningBolt(x) {
     x = lightningBolt;
-    return Math.random() < x
+    return Math.random() < x;
 };
 
+/* Tests for proc rolls */
 console.log("Earth Spike hit: " + rollEarthSpikes());
 console.log("Northern Winds hit: " + rollNorthernWinds());
 console.log("Lightning Bolt hit: " + rollLightningBolt());
+
+
+let hailstorm = false;
+let chargedStone = false;
+let tempest = false;
+let allSecondaryProcs = false;
+let noSecondaryProcs = false;
+
+function secondaryEffectProc() {
+    if(rollEarthSpikes() == true && rollNorthernWinds() == true && rollLightningBolt() == false) {
+        return hailstorm = true;
+    } else if (rollEarthSpikes() == false && rollNorthernWinds() == true && rollLightningBolt() == true) {
+        return tempest = true;
+    } else if (rollEarthSpikes() == true && rollNorthernWinds() == false && rollLightningBolt() == true) {
+        return chargedStone = true;
+    } else if (rollEarthSpikes() == true && rollNorthernWinds() == true && rollLightningBolt() == true) {
+        return allSecondaryProcs = true;
+    } else if (rollEarthSpikes() == false && rollNorthernWinds() == false && rollLightningBolt() == false) {
+        return noSecondaryProcs = true;
+    } else {
+        console.log('Something went wrong...');
+    }
+};
+
+secondaryEffectProc();
+console.log('Does Hailstorm proc? ' + hailstorm);
+console.log('Does Tempest proc? ' + tempest);
+console.log('Does Charged Stone proc? ' + chargedStone);
+console.log('Do all secondary effects proc? ' + allSecondaryProcs);
+console.log('Do none of the secondary effects proc? ' + noSecondaryProcs);

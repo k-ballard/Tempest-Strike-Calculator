09a3fb7943c436a3f638a4d9c034cb86581b8494
k-ballard <86800230+k-ballard@users.noreply.github.com>
2021-12-06 19:41:35 -0800

    Finally found a way to merge and add damage modifiers to the baseDamage propertygit add skills.js. Made calculateDamageModifiers() code more readable. Added crit chance, crit multiplier, and cooldown properties to the Skills class.

--- skills.js
+++ skills.js
@@ -1,38 +1,74 @@
 
-// Need to learn how to export this (make files modules)
+// Need to learn how to export this (turn files into modules)
 class Skills {
-    constructor(tags, baseDamage, damageModifiers, manaCost, attackSpeed) {
+    constructor(tags, baseDamage, damageModifiers, manaCost, attackSpeed, cooldown, critChance, critMulti) {
       this.tags = tags;
       this.baseDamage = baseDamage;
       this.damageModifiers = damageModifiers;
       this.manaCost = manaCost;
       this.attackSpeed = attackSpeed;
+      this.cooldown = cooldown;
+      this.critChance = critChance;
+      this.critMulti = critMulti;
     }
 
-    get newBaseDamage() {
+    get calculateBaseDamage() {
          return this.calculateDamageModifiers();
     }
     calculateDamageModifiers() {
-        // Attempt to calculate new damage with a For loop and an If condition
-          for (let i = 0; i < this.damageModifiers.length; i++) {
-                if (this.damageModifiers[i][0] === this.baseDamage[i][0]) {
-                    //this.baseDamage.push(this.damageModifiers[i][0]);
-                    return this.baseDamage[i][1] += this.damageModifiers[i][1];
-             }
-         }
-        
-    }
+        // Attempt to calculate new damage
+        let modifierLeng = this.damageModifiers.length;
+        let baseLeng = this.baseDamage.length;
+        let mods = this.damageModifiers;
+        let bases = this.baseDamage;
+        let emptyArr = [];
+          for (let i = 0; i < modifierLeng; i++) {
+            for (let j = 0; j < baseLeng; j++) {
+              //console.log(mods[i][0]);
+              //console.log(bases[j][0]);
+              if (mods[i][0] === bases[j][0]) {
+                //console.log("There's a damage type match.");
+                bases[j][1] += mods[i][1];
+                //console.log(bases);
+                //console.log(mods);
+              } else if (mods[i][0] !== bases[j][0] || bases[i] === undefined) {
+                  //console.log("These damage types don't match.")
+                  //console.log(bases);
+                  //console.log(mods);
+                  bases.push(mods[i]);
+                  //console.log(bases);
+                  //console.log(mods);
+              }
+          }
+          /*if (this.baseDamage[i] === undefined) {
+            console.log("We need to add this base damage type to the baseDamage property.");
+            return this.baseDamage.push(this.damageModifiers[i]);
+          }*/
+        }
+      }  
   }
   
 const hammerThrow = new Skills(
-    ['Physical','Throwing Attack','Strength','Dexterity'],
-    [['Physical', 18]],
-    [['Physical', 2],['Cold', 3]], 
-    0, 
-    1);
+  ['Physical','Throwing Attack','Strength','Dexterity'],
+  [['Physical', 18]],
+  [['Cold', 3],['Physical', 2],['Lightning', 1],['Physical', 5]], 
+  0, 
+  1,
+  0,
+  .05,
+  2);
 
+const smite = new Skills(
+  ['Fire','Spell','Attunement'],
+  [['Fire', 30]],
+  [],
+  3,
+  1,
+  0,
+  .05,
+  2);
 
+hammerThrow.calculateBaseDamage;
 
-console.log(hammerThrow.newBaseDamage);
-console.log(hammerThrow.baseDamage);
 console.log(hammerThrow.damageModifiers);
+console.log(hammerThrow.baseDamage);
